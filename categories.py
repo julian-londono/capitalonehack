a724dcc401de99aa5186cf11d83bfa04fcef737f
Julian Londono <jlond10@gmail.com>
2018-01-11 17:07:47 -0500

    Add blank json files

---
+++ categories.py
@@ -0,0 +1,15 @@
+import json, random
+with open('categorydata.json') as json_data:
+    data = json.load(json_data)
+
+itemList = {}
+for item in data["categories"]:
+    totalInCategory = len(item["hierarchy"])
+    if (totalInCategory > 1):
+        itemList[item["hierarchy"][totalInCategory-1]] = item["hierarchy"][0]
+
+# with open('categories.json', 'w') as outfile:
+#     json.dump(itemList, outfile)
+for x in range(100):
+    print(random.choice(itemList.keys())+ ": $" + str(random.randint(1,101)))
+  
