318cab6cd1ad80d004ac9c9b3ad9e335455f7644
Glenn Ren <glenn.r.phs@gmail.com>
2018-01-11 22:53:09 -0500

    Added bill payment functionality

--- custom/helpers.js
+++ custom/helpers.js
@@ -1,5 +1,53 @@
 const user_data = require('../userdata.json'); // static user data
 
+function getUserByName(name) {
+  return user_data.filter(
+      function(user_data){
+          return user_data.Users.name == name;
+      }
+  );
+}
+
+// returns bill object if bill exists
+// returns empty object if bill does not exist
+function checkBillExists(billType, bills) {
+    for (let i = 0; i < bills.length; i++) {
+        if (bills[i].type == billType) {
+            return bills[i];
+        }
+    }
+    return {};
+}
+
+function getBills(name, billType) {
+    let user = getUserByName(name); // get user object
+    let bills = user.bills; // get list of bill objects
+    let balance = user.balance; // get user balance
+    let current_bill = checkBillExists(billType, bills); // get current bill
+
+    let output = "";
+
+    // if the account has bills associated with it
+    if (bills.length) {
+        // if there are no parameters provided / current_bill is empty
+        if (Object.keys(current_bill).length === 0) {
+            output += 'Would you like to pay off all your bills?';
+        } else {
+            // if there is enough money to pay off the bill
+            if (current_bill.amount < balance) {
+                output += `Okay, I have paid off your ${billType} bill. Your balance has changed from ${balance} to`;
+                balance -= current_bill.amount;
+                output += `${balance}`;
+            } else {
+                output += `Sorry, this bill cannot be paid. You have a balance of ${balance}. The cost of the bill is ${current_bill.amount}.`;
+            }
+        }
+    } else {
+        output = 'There are no bills associated with this account.';
+    }
+    return output;
+}
+
 function getTrans(){
 
 }
@@ -38,3 +86,4 @@ function getLargePurchases(min){
 
 module.exports.getTrans = getTrans;
 module.exports.getLargePurchases = getLargePurchases;
+module.exports.getBills = getBills;
